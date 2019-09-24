53dd77998697b22a0a5cfff58b1ece493347a179
matnado <44616485+matnado@users.noreply.github.com>
2019-09-24 15:22:23 -0400

    Added new backbone detections methodologies and correted some typos

--- Codes_Backbone_detection/Filtering_methods_u.py
+++ Codes_Backbone_detection/Filtering_methods_u.py
@@ -32,10 +32,11 @@ class Network:
 		self.I = zip(I[:-1],I[1:]) 
 		if self.I[-1][0]==self.I[-1][1]:
 			del self.I[-1]
+		self.I_number = len(self.I)
 		
 
 	def Find_Intervals(self, x, N_I):
-		self.N_I = N_I #number of estimated intervals
+		self.I_number = N_I #number of estimated intervals
 		self.t_estimated = []#time between two successive intervals
 		
 		self.W_t = [0. for _ in itertools.repeat(None, self.T)] #total number of links per snapshot
@@ -54,10 +55,10 @@ class Network:
     			for t in range(self.T):
 	    			writer.writerow(['S', self.W_t[t], t, (t+0.999)])#I assumed that the time step is equal to 1. 
 	    	
-	    	if self.N_I>1:
+	    	if self.I_number>1:
 	    		a = []
-	    		while(len(a)<self.N_I):
-			    	os.system(path_bin+name_exe+' -i '+path_data+name_data_file+' -n '+str(self.T)+' -c '+str(self.N_I)+' -t 1 -o ./  > a.txt')#I assumed that the time step is equal to 1. 
+	    		while(len(a)<self.I_number):
+			    	os.system(path_bin+name_exe+' -i '+path_data+name_data_file+' -n '+str(self.T)+' -c '+str(self.I_number)+' -t 1 -o ./  > a.txt')#I assumed that the time step is equal to 1. 
 			    	myfile = open('a.txt', 'r')
 
 			    	for i in myfile:
@@ -67,8 +68,8 @@ class Network:
 			    		break
 			    		
 		    	self.t_estimated.append(0)
-	    		for i in range(self.N_I):
-	    			self.t_estimated.append(int(a[self.N_I-1-i]))
+	    		for i in range(self.I_number):
+	    			self.t_estimated.append(int(a[self.I_number-1-i]))
 	    		self.t_estimated.append(self.T)
 		else:
 			self.t_estimated.append(0)#first time is 0
@@ -96,6 +97,15 @@ class Network:
 		    self.links[(x[i][1], x[i][2])] = None
 		    
 		self.tot_strength = 2*np.sum(self.wo.values())
+		
+	def degree(self, x):
+		self.degree = {} #degree of each node in the overall network evolution
+		
+		for i,j in self.links:
+		    self.degree.setdefault(i, 0), self.degree.setdefault(j, 0)  
+		    self.degree[i] += 1
+		    self.degree[j] += 1
+
 	
         def compute_weEADM_Accuracy(self, x):
 		#only to plot W(t) as a function of time
@@ -114,7 +124,42 @@ class Network:
 		    tot = sq.sum()
 		    self.weEADM+=np.outer(sq,sq)/(float(sq.sum())-1) 
 		    
-		
+	
+	
+	def compute_p_ij_I(self, x):
+		self.p_ij_I = {}  #it accounts for the interval division
+		for i,j in self.links:
+			self.p_ij_I.setdefault((i, j), 0.), self.p_ij_I.setdefault((j, i), 0.)
+
+		for i,j in self.I:
+		    sq = np.zeros(self.N)
+		    wij_I = {}
+		    if int(j)==self.T-1:
+				ed = x[(x[:,0]>=i)*(x[:,0]<=j),1:]
+		    else:	
+				ed = x[(x[:,0]>=i)*(x[:,0]<j),1:]
+		    
+		    if len(ed)==0: continue
+		    
+		    for k in range(len(ed)):#it can be improved
+		    	#print ed[k][0], ed[k][1]
+		    	wij_I.setdefault((ed[k][0], ed[k][1]), 0.)
+		    	wij_I[(ed[k][0], ed[k][1])] += 1.
+		    
+		    r = Counter(ed[:,0])
+		    sq[np.array(r.keys())] = r.values()
+		    r = Counter(ed[:,1])
+		    sq[np.array(r.keys())] += r.values()
+
+		    for l,k in wij_I:
+		    	self.p_ij_I[(l,k)] += wij_I[l, k]/sq[l]*(j-i)
+		    	self.p_ij_I[(k,l)] += wij_I[l, k]/sq[k]*(j-i)
+		    	
+		for i,j in self.links:
+		    	self.p_ij_I[(i, j)] = self.p_ij_I[(i, j)]/self.T
+		    	self.p_ij_I[(j, i)] = self.p_ij_I[(j, i)]/self.T
+		    	
+	
 	def compute_weEADM(self, x):	
 		self.weEADM = {}#expected weigth according to the EADM
 		for i,j in self.links:
@@ -161,8 +206,43 @@ class Network:
 		
 		self.optimal_activity = {} #key 1: source node, value: optimal value of the activity 
 		self.m_ij_o = defaultdict(dict) #key 1: source node, key 2: target node, value: weight WEIGHTS CAN BE ZERO
-		    
-        
+	
+	def initialize_variables_likelihood_I(self, x):
+		#only to plot W(t) as a function of time
+		self.tau = []
+		self.w_ij_I = []
+		self.strength_a_I = [{} for i in range(self.I_number)]
+		self.strength_a_I_node = defaultdict(dict)
+		self.tot_strength_I = []
+		
+		self.exp = 0.
+		
+		num = 0
+		ss = 0
+		
+		for i,j in self.I:
+			self.tau.append(j-i)
+			sq = np.zeros(self.N)
+			if int(j)==self.T-1:#TODO
+				ed = x[(x[:,0]>=i)*(x[:,0]<=j),1:]
+			else:	
+				ed = x[(x[:,0]>=i)*(x[:,0]<j),1:]
+		    	if len(ed)==0: continue
+			self.w_ij_I.append(Counter(map(tuple,ed[:,:])))
+			r = Counter(ed[:,0])
+			sq[np.array(r.keys())] = r.values()
+			r = Counter(ed[:,1])
+			sq[np.array(r.keys())] += r.values()
+			for k in range(self.N):	
+				self.strength_a_I[num][k] = sq[k]
+				self.strength_a_I_node[k][num] = sq[k]
+			self.tot_strength_I.append(sq.sum())
+			ss += np.sum(self.w_ij_I[num].values())
+			num+=1
+		self.tau[-1]+=1 #due to the ceil function used in Bayesian function. In this way np.sum(self.tau)==self.T
+
+		self.strength_a = [np.sum(self.strength_a_I_node[i].values()) for i in range(len(self.strength_a_I_node))]	    
+        	self.optimal_activity_I = [{} for i in range(self.I_number)] #index 1: interval number, key 1: source node, value: optimal value of the activity 
         
         def compute_expected_weigths_TFM(self):
         	self.weTFM = np.zeros((len(self.optimal_activity), len(self.optimal_activity)))
@@ -228,6 +308,87 @@ class Network:
 		for source_node in self.m_ij_o:
 		    self.optimal_activity[source_node] = float(z[i])
 		    i+=1
+		    
+	'Optimal value of the activity, according to ML estimation'	
+	def estimate_activity_I(self, t):
+			z_Guess = []
+			activity_guess = {}
+			self.T = self.tau[t]
+			
+			for source_node in self.m_ij_o:
+			    activity_guess[source_node] = np.min([float(self.strength_a_I[t][source_node])/np.sqrt((self.tot_strength_I[t])*self.tau[t]), 0.999])
+
+			for source_node in self.m_ij_o:
+			    z_Guess.append(float(activity_guess[source_node]))
+			
+			if len(self.reduced_nodes)>0:
+				results =least_squares(self.Functions_to_be_optimized_I, z_Guess, bounds=(0, 1))#better than fsolve
+				z = results.x
+			    
+			i=0
+			for source_node in self.m_ij_o:
+				self.optimal_activity_I[t][source_node] = float(z[i])
+			    	i+=1
+			    	
+	def compute_expected_weigths_ETFM(self):#it can be optimized by considering only the number of nodes in the given interval
+        	self.weETFM = {}
+        	for t in range(self.I_number):
+			for i in self.optimal_activity_I[t]:
+				for j in self.optimal_activity_I[t]:
+					if i<j:
+						self.weETFM.setdefault((i,j), 0) 
+						self.weETFM[(i,j)] += self.optimal_activity_I[t][i]*self.optimal_activity_I[t][j]*self.tau[t]
+	
+		for i,j in self.links:
+			self.weETFM.setdefault((i,j), 0)
+        
+        def initialize_m_ij_o_I(self, t):
+        		self.optimal_activity = {} #key 1: source node, value: optimal value of the activity 
+			self.m_ij_o = defaultdict(dict) #key 1: source node, key 2: target node, value: weight WEIGHTS CAN BE ZERO
+			self.reduced_nodes = {}
+			for i in self.strength_a_I[t]:
+				if self.strength_a_I[t][i]>0.:
+					self.reduced_nodes[i]=None
+			
+			#print len(self.reduced_nodes)
+			
+			for i in self.reduced_nodes:
+				for j in self.reduced_nodes:
+					if i<j:
+						self.w_ij_I[t].setdefault((i,j),0)
+		            			self.m_ij_o[i][j] = self.w_ij_I[t][(i,j)]
+			        	        
+			        	        
+			for i in self.reduced_nodes:
+		    		for j in self.reduced_nodes:
+		        		if j<i:
+		        			self.m_ij_o[i][j] = self.m_ij_o[j][i]
+		        			
+	
+	
+	def function_source_I(self, source_node):
+		supp= 0.
+		for target_node in self.m_ij_o[source_node]:
+		    if target_node != source_node:
+		        supp += float(self.m_ij_o[source_node][target_node] - float(self.T)*self.optimal_activity[source_node]*self.optimal_activity[target_node])/(1. - self.optimal_activity[source_node]*self.optimal_activity[target_node])
+		return supp
+
+	'N fuctions, one for each node, optimized according to the ML'	
+	def Functions_to_be_optimized_I(self, z):
+		i = 0
+		for source_node in self.m_ij_o:
+			self.optimal_activity[source_node] = float(z[i])
+		        i+=1
+		        
+		tot_equations = len(self.m_ij_o)
+		H = np.empty((tot_equations))
+		    
+		i = 0
+		for source_node in self.m_ij_o:
+		    H[i] = self.function_source_I(source_node)#
+		    i+=1
+		
+		return H 
 			    			
         
 ########################################################################################################
@@ -288,7 +449,7 @@ def Relative_error_weEADM_Accuracy(Net, myfile_relative_error, dt):
 		wo += Net.wo[i,j]
 
 	
-	myfile_relative_error.write(str(alpha)+'	'+str(RE(wo, weEADM))+'\n')	
+	myfile_relative_error.write(str(dt)+'	'+str(RE(wo, weEADM))+'\n')	
 	
 #######################################################################################################################
 def Relative_error_weTFM(Net, myfile_relative_error, dt):
@@ -305,7 +466,22 @@ def Relative_error_weTFM(Net, myfile_relative_error, dt):
 	for i,j in Net.wo:
 		wo += Net.wo[i,j]
 
-	myfile_relative_error.write(str(alpha)+'	'+str(RE(wo, weTFM))+'\n')	
+	myfile_relative_error.write(str(dt)+'	'+str(RE(wo, weTFM))+'\n')
+	
+#######################################################################################################################
+def Relative_error_weETFM(Net, myfile_relative_error, dt):
+	#Relative Error
+	RE = lambda th,exp: float(abs(th-exp))/th
+	
+	wo = 0.
+	weETFM = 0.
+	for i,j in Net.weETFM:
+		weETFM += Net.weETFM[i,j]
+				
+	for i,j in Net.wo:
+		wo += Net.wo[i,j]
+
+	myfile_relative_error.write(str(dt)+'	'+str(RE(wo, weETFM))+'\n')	
 
 ########################################################################################################
 def links_TFM(Net, myfile_links, myfile_edgelist, alpha, Bonferroni_corr, dt):	
@@ -336,6 +512,49 @@ def links_EADM(Net, myfile_links, myfile_edgelist, alpha, Bonferroni_corr, dt):
 	myfile_links.write(str(alpha)+'	'+str(len(edEADM))+'\n')
 	
 ########################################################################################################
+def links_ETFM(Net, myfile_links, myfile_edgelist, alpha, Bonferroni_corr, dt):	
+	if Bonferroni_corr=='yes':
+		nl = len(Net.links)#total number of links in the network
+	else:
+		nl = 1. #no Bonferroni correction!
+	
+	edETFM =  [(i,j) for i,j in Net.links if st.poisson.sf(Net.wo[i,j]-1,Net.weETFM[i,j])<alpha/nl]
+	
+	for i in range(len(edETFM)):
+		myfile_edgelist.write(str(edETFM[i][0])+'	'+str(edETFM[i][1])+'\n')
+	
+	myfile_links.write(str(alpha)+'	'+str(len(edETFM))+'\n')
+
+#################################################################################
+def compute_p_value_TDF(pij, ki):
+	return pow(1.-pij, ki - 1.)  
+	
+	
+########################################################################################################
+def links_TDF(Net, myfile_links, myfile_edgelist, alpha, Bonferroni_corr, dt):	
+	if Bonferroni_corr=='yes':
+		nl = len(Net.links)#total number of links in the network
+	else:
+		nl = 1. #no Bonferroni correction!
+	
+	pv_TDF = {}
+	for i,j in Net.links: 
+		pv_ij = compute_p_value_TDF(Net.p_ij_I[(i,j)], Net.degree[i]) 
+		pv_ji = compute_p_value_TDF(Net.p_ij_I[(j,i)], Net.degree[j])
+		if pv_ij <= pv_ji:
+			pv_TDF[(i,j)] = pv_ij
+		else:
+			pv_TDF[(i,j)] = pv_ji
+			
+	edTDF = [(i,j) for i,j in Net.links if pv_TDF[(i,j)]<alpha/nl]
+	
+	for i in range(len(edTDF)):
+		myfile_edgelist.write(str(edTDF[i][0])+'	'+str(edTDF[i][1])+'\n')
+	
+	myfile_links.write(str(alpha)+'	'+str(len(edTDF))+'\n')
+
+	
+########################################################################################################
 def links_SVN(Net, myfile_links, myfile_edgelist, alpha, Bonferroni_corr, dt):	
 	if Bonferroni_corr=='yes':
 		nl = len(Net.links)#total number of links in the network
@@ -351,7 +570,7 @@ def links_SVN(Net, myfile_links, myfile_edgelist, alpha, Bonferroni_corr, dt):
 	
 ########################################################################################################
 'it is faster for small networks and  weEADM is equal to wo. For very large networks (N>10^5 or 10^4) using this code Memory Error occurs.'
-def Backbone_detection(path_in = 'DATASET/Example_PrimarySchool/', name_file='primaryschool.csv', sep='\t', dt=[20., 60.*1., 60.*5., 60.*15.], multiedges = ['no'], directory_out = 'Output', column_time=0, remove_nights='yes', alpha = [0.01], Bonferroni_corr = 'yes', model = 'EADM_BB', N_I = 1.):
+def Backbone_detection(path_in = 'DATASET/Example_PrimarySchool/', name_file='primaryschool.csv', sep='\t', dt=[60.*5.], multiedges = ['no'], directory_out = 'Output', column_time=0, remove_nights='yes', alpha = [0.01], Bonferroni_corr = 'yes', model = 'EADM_BB', N_I = 1):
 	time_start = time.time() 
 	
 	if multiedges == ['no']:
@@ -367,23 +586,34 @@ def Backbone_detection(path_in = 'DATASET/Example_PrimarySchool/', name_file='pr
 	for i in range(len(dt)):
 		myfile_links = open(path_out+'/links_'+str(int(dt[i]))+'_'+model+'.txt','w')
 		
-		if model=='EADM_BB_Accuracy' or model=='EADM_I_Accuracy' or model=='TFM':
+		if model=='EADM_BB_Accuracy' or model=='EADM_I_Accuracy' or model=='TFM' or model=='ETFM_I' or model=='ETFM_BB':
 			myfile_relative_error = open(path_out+'/relative_error_'+str(int(dt[i]))+'_'+model+'.txt','w')
 		
 		x, N = Read_file(path_in, name_file, multiedges[i], dt[i], column_time, sep, remove_nights)
 		Net = Network(N, x)
 		Net.weights_links(x)
+		Net.degree(x)
 		
-		if model=='EADM_BB' or model=='EADM_BB_Accuracy':
+		if model=='EADM_BB' or model=='EADM_BB_Accuracy' or model=='ETFM_BB' or model == 'TDF_BB':
 			Net.Bayesian(x)
-		elif model=='EADM_I' or model=='EADM_I_Accuracy':
+		elif model=='EADM_I' or model=='EADM_I_Accuracy' or model=='ETFM_I' or model == 'TDF_I':
 			Net.Find_Intervals(x, N_I)
+		
 		
 		if model=='EADM_BB_Accuracy' or model=='EADM_I_Accuracy':
 			Net.compute_weEADM_Accuracy(x) 
 			Relative_error_weEADM_Accuracy(Net, myfile_relative_error, dt[i])
 		elif model=='EADM_BB' or model=='EADM_I':
-			Net.compute_weEADM(x)	
+			Net.compute_weEADM(x)
+		elif model=='ETFM_BB' or model=='ETFM_I':
+			Net.initialize_variables_likelihood_I(x)
+			for t in range(Net.I_number):
+				Net.initialize_m_ij_o_I(t)
+				Net.estimate_activity_I(t)
+			Net.compute_expected_weigths_ETFM()
+			Relative_error_weETFM(Net, myfile_relative_error, dt[i])
+		elif model=='TDF_BB' or model=='TDF_I':
+			Net.compute_p_ij_I(x)	
 		elif model=='SVN':
 			Net.compute_strength_SVN(x)
 		elif model=='TFM':	
@@ -393,7 +623,7 @@ def Backbone_detection(path_in = 'DATASET/Example_PrimarySchool/', name_file='pr
 			Net.compute_expected_weigths_TFM()
 			Relative_error_weTFM(Net, myfile_relative_error, dt[i])
 		else:
-			sys.exit('These are the acceptable keywords: TFM, SVN, EADM_BB, EADM_I, EADM_I_Accuracy, or EADM_BB_Accuracy. In the ReadMe file we explain their meaning')
+			sys.exit('These are the acceptable keywords: \nTFM, \nSVN, \nEADM_BB, \nETMF_BB, \nTDF_BB, \nEADM_I, \nETMF_I, \nTDF_I, \nEADM_I_Accuracy, or \nEADM_BB_Accuracy. \nIn the ReadMe file we explain their meaning')
 			
 		for j in range(len(alpha)):	
 			myfile_edgelist = open(path_out+'/edgelist_'+str(int(dt[i]))+'_'+str(alpha[j])+'_'+model+'.txt','w')
@@ -401,12 +631,16 @@ def Backbone_detection(path_in = 'DATASET/Example_PrimarySchool/', name_file='pr
 				links_EADM(Net, myfile_links, myfile_edgelist, alpha[j], Bonferroni_corr, dt[i])
 			elif model=='EADM_BB' or model=='EADM_I':
 				links_EADM(Net, myfile_links, myfile_edgelist, alpha[j], Bonferroni_corr, dt[i])
+			elif model=='ETFM_BB' or model=='ETFM_I':
+				links_ETFM(Net, myfile_links, myfile_edgelist, alpha[j], Bonferroni_corr, dt[i])
+			elif model=='TDF_BB' or model=='TDF_I':
+				links_TDF(Net, myfile_links, myfile_edgelist, alpha[j], Bonferroni_corr, dt[i])
 			elif model=='SVN':
 				links_SVN(Net, myfile_links, myfile_edgelist, alpha[j], Bonferroni_corr, dt[i])
 			elif model=='TFM':	
 				links_TFM(Net, myfile_links, myfile_edgelist, alpha[j], Bonferroni_corr, dt[i])
 			else:
-				sys.exit('These are the acceptable keywords: TFM, SVN, EADM_BB, EADM_I, EADM_I_Accuracy, or EADM_BB_Accuracy. In the ReadMe file we explain their meaning')
+				sys.exit('These are the acceptable keywords: \nTFM, \nSVN, \nEADM_BB, \nETMF_BB, \nTDF_BB, \nEADM_I, \nETMF_I, \nTDF_I, \nEADM_I_Accuracy, or \nEADM_BB_Accuracy. \nIn the ReadMe file we explain their meaning')
 			
 		 	myfile_edgelist.close()
 		 	
@@ -414,7 +648,7 @@ def Backbone_detection(path_in = 'DATASET/Example_PrimarySchool/', name_file='pr
 		
 		
 	
-		if model=='EADM_BB_Accuracy' or model=='EADM_I_Accuracy' or model=='TFM':
+		if model=='EADM_BB_Accuracy' or model=='EADM_I_Accuracy' or model=='TFM' or model=='ETFM_I' or model=='ETFM_BB':
 			myfile_relative_error.close()
 	
 	elapsed = time.time() - time_start
